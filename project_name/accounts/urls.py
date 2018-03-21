a97d1affd0bfe8dd042a73671f930bd0afe70a6b
Tiago Silva <tiago.silva@incoproip.com>
2018-03-21 15:16:25 +0000

    Celery settings updated

--- project_name/accounts/urls.py
+++ project_name/accounts/urls.py
@@ -3,10 +3,5 @@ from django.conf.urls import url
 import accounts.views
 
 accounts_urlpatterns = [
-    url(r'^add-user/$', accounts.views.RegisterProfileView.as_view(), name='add-user'),
-    url(r'^view/(?P<slug>[a-zA-Z0-9-_]+)/', accounts.views.ProfileView.as_view(), name="view-profile"),
-    url(r'^edit/(?P<slug>[a-zA-Z0-9-_]+)/', accounts.views.ProfileEditView.as_view(), name="edit-profile"),
-    url(r'^(?P<slug>[a-zA-Z0-9-_]+)/change-password/', accounts.views.ChangePasswordView.as_view(), name="change-password"),
-    url(r'^list-users/$', accounts.views.ListUsersView.as_view(), name='list-users'),
-    url(r'^settings/(?P<slug>[a-zA-Z0-9-_]+)/$', accounts.views.SettingsView.as_view(), name='settings'),
+    url(r'^user/create/$', accounts.views.RegisterProfileView.as_view(), name='add-user'),
 ]
